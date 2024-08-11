455b75981d883483687d2a4d35a8a2e70c1382c2
João <ut7vitor60@gmail.com>
2024-08-11 19:33:17 -0300

    Add. Dynamic Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-function Header(){
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+      {description} Fundamental React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
 }
 
 function App() {
   return (
     <div>
+      <Header />
       <main>
-        <Header></Header>
         <h2>Time to get started!</h2>
-        <Mensagem/>
       </main>
     </div>
   );
 }
-
-function Mensagem(){
-  return(
-    <button>Clique aqui</button>
-  )
-}
-
-
 
 export default App;
