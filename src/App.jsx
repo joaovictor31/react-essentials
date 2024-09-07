6eeb1b9e3d1137a63116137dd5fc3118009c57c7
João <ut7vitor60@gmail.com>
2024-09-07 16:21:43 -0300

    chamando dados de data.js e adicionando links ao clicar

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import {CORE_CONCEPTS} from './data.js';  {/* coloco entre chave pq não é um d
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   
 
@@ -50,8 +51,17 @@ function App() {
           <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
           <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
         </menu>
+        <div id = 'tab-content'>
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+            {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+        </div>
         </section>
-      {selectedTopic}
+      
       </main>
     </div>
   );
